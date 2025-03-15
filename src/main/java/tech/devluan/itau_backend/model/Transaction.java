6ecca69042537c08d5f20331015a632997c929f9
devluan <contatoluansantos7@gmail.com>
2025-03-14 22:42:33 -0300

    feat: create Transaction

--- src/main/java/tech/devluan/itau_backend/model/Transaction.java
+++ src/main/java/tech/devluan/itau_backend/model/Transaction.java
@@ -1,5 +1,12 @@
 package tech.devluan.itau_backend.model;
 
-public class Transaction {
+import lombok.Getter;
+import lombok.Setter;
+import java.time.OffsetDateTime;
 
+@Getter
+@Setter
+public class Transaction {
+    private Double valor;
+    private OffsetDateTime dataHora;
 }
